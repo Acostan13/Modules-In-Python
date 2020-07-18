8a48b6466b10cff1a4a05e44a53efbbacebab28a
Alex Costan <acostan.con@orangetheory.com>
2020-07-18 10:04:17 -0400

    packages in python

---
+++ main.py
@@ -0,0 +1,9 @@
+import utility
+from shopping import shopping_cart
+
+print(utility)  # => <module 'utility' from 'E:\\code\\Modules In Python\\utility.py'>
+print(utility.multiply(2, 3))  # => 6
+print(utility.divide(2, 3))  # => 0.666666666666666
+
+print(shopping_cart)  # <module 'shopping.shopping_cart' from 'E:\\code\\Modules In Python\\shopping\\shopping_cart.py'>
+print(shopping_cart.buy('apple'))  # ['apple']
