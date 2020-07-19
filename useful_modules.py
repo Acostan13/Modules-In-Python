c27c345a3a0cfd5eef7148e4f3d729b5b4c79220
Alex Costan <acostan.con@orangetheory.com>
2020-07-19 10:28:33 -0400

    useful modules

---
+++ useful_modules.py
@@ -0,0 +1,29 @@
+from collections import Counter, defaultdict, OrderedDict
+
+li = [1, 2, 3, 4, 5, 6, 7, 7]
+sentence = 'blah blah thinking about python'
+print(Counter(li))  # Counter({7: 2, 1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1})
+print(Counter(sentence))  # Counter({'h': 4, ' ': 4, 'b': 3, 'a': 3, 't': 3, 'n': 3, 'l': 2, 'i': 2, 'o': 2, 'k': 1, 'g': 1, 'u': 1, 'p': 1, 'y': 1})
+
+
+dictionary = defaultdict(int, {'a': 1, 'b': 2})
+print(int())  # 0
+
+dictionary2 = defaultdict(lambda: 5, {'a': 1, 'b': 2})
+print(dictionary2['c'])  # 5
+
+
+d = OrderedDict()
+d['a'] = 1
+d['b'] = 2
+
+d2 = OrderedDict()
+d2['a'] = 1
+d2['b'] = 2
+
+d3= OrderedDict()
+d3['b'] = 2
+d3['a'] = 1
+
+print(d2 == d)  # True
+print(d3 == d2)  # False
